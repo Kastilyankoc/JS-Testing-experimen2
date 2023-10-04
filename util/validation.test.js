da531eb38acfc654f9f75171ce1dc856ce068bee
Kastilyankoc <132671325+Kastilyankoc@users.noreply.github.com>
2023-10-04 21:35:21 +0300

    Add files via upload

---
+++ util/validation.test.js
@@ -0,0 +1,23 @@
+import { it, expect } from 'vitest';
+import { validateNotEmpty } from './validation';
+
+it('should throw an error of if an empty string is provided as a value', () => {
+  const testInput = '';
+  const validationFn = () => validateNotEmpty(testInput);
+
+  expect(validationFn).toThrow();
+});
+it('should throw an error of if an empty string is provided as a value', () => {
+  const testInput = '   ';
+  const validationFn = () => validateNotEmpty(testInput);
+
+  expect(validationFn).toThrow();
+});
+
+it('should throw an error with the provided error message', () => {
+  const testInput = '';
+  const testErrorMessage = 'Test';
+  const validationFn = () => validateNotEmpty(testInput, testErrorMessage);
+
+  expect(validationFn).toThrow(testErrorMessage);
+});
